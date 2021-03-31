20178971cc51e2965929743bfcdff49110e6fe4f
Patryk Rzucidlo (PTKDev) <ptkdev@gmail.com>
2021-03-31 11:48:14 +0200

    [Fix] JSDoc

--- app/functions/module.ts
+++ app/functions/module.ts
@@ -15,28 +15,13 @@ import type { ModuleInterface, ModuleResponseInterface } from "@app/types/module
  * Hello World
  * =====================
  *
- * Print hello-world, run with: `npx @ptkdev/node-cli-boilerplate`
+ * Print hello-world, run with: npx @ptkdev/node-cli-boilerplate
  *
  * @interface [ModuleInterface ModuleResponseInterface](https://github.com/ptkdev-boilerplate/node-cli-boilerplate/blob/main/app/types/module.type.ts)
  *
  * @param {string} {text} - input string
  *
  * @return {Promise<ModuleResponseInterface>} (async) app() function that return string
- *
- * @example
- * 1. In your node project run: `npm install @ptkdev/node-cli-boilerplate@latest --save`
- * 2. Usage (async):
- *
- * ```typescript
- * import m from "@ptkdev/node-cli-boilerplate";
- *
- * (async () => {
- * 	 const { app } = await m({ text: "hello-world" });
- *
- *	 console.log(app());
- * })();
- *
- * ```
  *
  */
 const m = async ({ text }: ModuleInterface): Promise<ModuleResponseInterface> => {
