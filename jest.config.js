e6764d48cd2f38e220bd366e92d3aeebcb5cee8c
Patryk Rzucidlo (@PTKDev) <ptkdev@gmail.com>
2023-02-25 04:44:37 +0100

    [Fix] Jest with tsconfig

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,7 @@
 export default {
 	preset: "ts-jest",
 	testEnvironment: "node",
-	globals: {
-		"ts-jest": {
-			tsconfig: "tsconfig.json",
-		},
+	transform: {
+		"^.+\\.ts?$": ["ts-jest", { tsconfig: "tsconfig.json" }],
 	},
 };
